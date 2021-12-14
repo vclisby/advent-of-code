626389bde2e06a25d58c70df0093f8f189e702ca
Victor <victor.clisby@hotmail.com>
2021-12-15 08:31:48 +1030

    Utility functions to convert inputs to string or numbers

---
+++ 2021/utilities.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+const INPUT_FILENAME = 'input.txt';
+
+function convertInputToStringArray(dir) {
+    return fs.readFileSync(path.join(dir, INPUT_FILENAME), 'utf8').toString().trim().split('\n');
+}
+
+function convertInputToNumberArray(dir) {
+    return convertInputToStringArray(dir).map((num) => parseInt(num, 10));
+}
+
+module.exports = {
+    convertInputToStringArray,
+    convertInputToNumberArray,
+};
