b7dfcd4ba23495249e2ac67610a02cdace45d27c
Victor <victor.clisby@hotmail.com>
2021-12-15 09:41:22 +1030

    Remove newlines when parsing input

--- 2021/utilities.js
+++ 2021/utilities.js
@@ -4,7 +4,12 @@ const fs = require('fs');
 const INPUT_FILENAME = 'input.txt';
 
 function convertInputToStringArray(dir) {
-    return fs.readFileSync(path.join(dir, INPUT_FILENAME), 'utf8').toString().trim().split('\n');
+    return fs
+        .readFileSync(path.join(dir, INPUT_FILENAME), 'utf8')
+        .toString()
+        .trim()
+        .split('\n')
+        .map((value) => value.trimEnd());
 }
 
 function convertInputToNumberArray(dir) {
